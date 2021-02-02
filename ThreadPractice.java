8fc97dd2b107efe0d6df66d02e96fdbf2c8ea622
TechWZ <TechArtisan@126.com>
2021-02-02 18:07:09 +0800

    update

--- ThreadPractice.java
+++ ThreadPractice.java
@@ -1,3 +1,23 @@
 public class ThreadPractice {
-    
+    public static void main(String[] args) {
+        System.out.println("main start...");
+        Thread t = new Thread() {
+            public void run() {
+                System.out.println("Thread run...");
+                try {
+                    Thread.sleep(10);    
+                } catch (Exception e) {
+                    //TODO: handle exception
+                }
+                System.out.println("Thread end.");
+            };
+        };
+        t.start();
+        try {
+            Thread.sleep(20);
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+        System.out.println("main end...");
+    }
 }
